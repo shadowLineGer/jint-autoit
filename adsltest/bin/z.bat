da5a2e5e67c11094adf7b1dae12c82908d8eae28
today.xa <today.xa@a941e64f-6271-e1c7-cb6b-a99a4888a98a>
2011-02-11 03:56:28 +0000

    调整目录结构

--- adsltest/bin/z.bat
+++ adsltest/bin/z.bat
@@ -1,15 +1,15 @@
-@echo "Clear"
-@del AutoTest.exe pingtest.exe qx_manager.exe 
+@echo [Clear]
+@del AutoTest.exe pingtest.exe qx_manager.exe log.log
 
-@echo "Compile Script to .exe"
+@echo [Compile Script to .exe]
 @"C:\Program Files\AutoIt3\Aut2Exe\Aut2exe.exe" /in ..\AutoTest.au3 /out .\AutoTest.exe /icon ..\favicon.ico /nodecompile /comp 4
 @"C:\Program Files\AutoIt3\Aut2Exe\Aut2exe.exe" /in ..\pingtest.au3 /out .\pingtest.exe /icon ..\favicon.ico /nodecompile /comp 4
 @"C:\Program Files\AutoIt3\Aut2Exe\Aut2exe.exe" /in ..\qx_manager.au3 /out .\qx_manager.exe /icon ..\favicon.ico /nodecompile /comp 4
 
-@echo "compress files"
+@echo [compress files]
 @7za a -y update.7z ..\page_check.js pingtest.exe qx_manager.exe client.ini
 
-@echo "Deploy"
+@echo [Deploy]
 @copy AutoTest.exe update.zip
 @move update.zip ..\..\..\jint-gae\kuandaiceshi\img\
 @move update.7z ..\..\..\jint-gae\kuandaiceshi\img\
